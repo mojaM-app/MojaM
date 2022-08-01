e4568bd0fb951dfce4ec009595060127a7738e60
mojaM-app <miriam.rzeszow@gmail.com>
2022-08-01 21:39:25 +0200

    fix layout on desktop

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,18 +1,23 @@
 import { ChangeDetectionStrategy, Component, ViewChild } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
+import { DeviceService } from 'src/services/device/device.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
   @ViewChild('sidenav') public sidenav: MatSidenav | undefined;
-  events: string[] = [];
-  opened: boolean = false;
 
-  closeSidenav() {
+  public isMobile: boolean = true;
+
+  public constructor(deviceService: DeviceService) {
+    this.isMobile = deviceService.isMobile();
+  }
+
+  public closeSidenav(): void {
     this.sidenav?.close();
   }
 }
