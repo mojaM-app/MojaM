7b9447d72ffdab488f3ec5144c6a5e5aeeb56a38
mojaM-app <miriam.rzeszow@gmail.com>
2022-08-04 20:46:29 +0200

    fix RWD

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: () => import('./components/news/news.module').then(m => m.NewsModule)},
-  { path: 'news', redirectTo: '' },
+  { path: '', redirectTo: 'news', pathMatch: 'full' },
+  { path: 'news', loadChildren: () => import('./components/news/news.module').then(m => m.NewsModule)},
   { path: 'bulletin', loadChildren: () => import('./components/bulletin/bulletin.module').then(m => m.BulletinModule)},
   { path: 'community', loadChildren: () => import('./components/community/community.module').then(m => m.CommunityModule)},
+
   { path: 'not-found', loadComponent: () => import('./components/static/not-found/not-found.component').then(m => m.NotFoundComponent)},
   { path: '**', redirectTo: '/not-found' }
 ];
--- frontend/src/app/components/news/news.module.ts
+++ frontend/src/app/components/news/news.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { InformationComponent } from './information/information.component';
+import { CalendarComponent } from './calendar/calendar.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    InformationComponent,
+    CalendarComponent
+  ],
   imports: [
     CommonModule
   ]
