a78718439511cc629346643e8498dcd1cfece407
mojaM-app <miriam.rzeszow@gmail.com>
2022-08-13 21:27:34 +0200

    new function WithUnsubscribeOnDestroy

---
+++ frontend/src/utils/with-unsubscribe-on-destroy.ts
@@ -0,0 +1,28 @@
+import { OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+export class Empty {}
+export type Constructor<T = Empty> = new (...args: any[]) => T;
+
+export function WithUnsubscribeOnDestroy<T extends Constructor = Constructor<Empty>>(Base: T = Empty as T) {
+  return class extends Base implements OnDestroy {
+    private _subscriptions: Subscription = new Subscription();
+
+    public ngOnDestroy(): void {
+      const baseOnDestroy: () => void = super['ngOnDestroy'];
+      if (baseOnDestroy) {
+        baseOnDestroy.bind(this)();
+      }
+      this.unsubscribe();
+    }
+
+    public unsubscribe() {
+      this._subscriptions.unsubscribe();
+      this._subscriptions = new Subscription();
+    }
+
+    public registerSubscription(subscription: Subscription) {
+      this._subscriptions.add(subscription);
+    }
+  };
+}
