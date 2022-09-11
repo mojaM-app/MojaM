d9f6907ab4b34a2d839de0725c5dec42237d5df5
mojaM-app <miriam.rzeszow@gmail.com>
2022-09-11 12:40:03 +0200

    use TouchDirective

--- frontend/src/app/components/community/index/index.component.ts
+++ frontend/src/app/components/community/index/index.component.ts
@@ -13,48 +13,17 @@ export class IndexComponent {
 
   public selectedTab: number = 0;
 
-  private swipeCoord?: [number, number];
-  private swipeTime?: number;
-
   public constructor() {}
 
-  swipe(e: TouchEvent, when: string): void {
-    const coord: [number, number] = [
-      e.changedTouches[0].clientX,
-      e.changedTouches[0].clientY,
-    ];
-    const time = new Date().getTime();
+  public selectNextTab(): void {
+    if (this.selectedTab < (this.tabGroup?._tabs?.length ?? 0) - 1) {
+      this.selectedTab++;
+    }
+  }
 
-    if (when === 'start') {
-      this.swipeCoord = coord;
-      this.swipeTime = time;
-    } else if (when === 'end' && this.swipeCoord) {
-      const direction = [
-        coord[0] - this.swipeCoord[0],
-        coord[1] - this.swipeCoord[1],
-      ];
-      const duration = time - (this.swipeTime ?? 0);
-
-      if (
-        duration < 1000 && //
-        Math.abs(direction[0]) > 30 && // Long enough
-        Math.abs(direction[0]) > Math.abs(direction[1] * 3)
-      ) {
-        // Horizontal enough
-        const swipe = direction[0] < 0 ? 'next' : 'previous';
-        switch (swipe) {
-          case 'previous':
-            if (this.selectedTab > 0) {
-              this.selectedTab--;
-            }
-            break;
-          case 'next':
-            if (this.selectedTab < (this.tabGroup?._tabs?.length ?? 0) - 1) {
-              this.selectedTab++;
-            }
-            break;
-        }
-      }
+  public selectPrevTab(): void {
+    if (this.selectedTab > 0) {
+      this.selectedTab--;
     }
   }
 }
