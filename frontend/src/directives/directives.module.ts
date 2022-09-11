00692da6e3c40cf2bc4032099ff0e75a09c7da18
mojaM-app <miriam.rzeszow@gmail.com>
2022-09-11 12:39:26 +0200

    add TouchDirective

--- frontend/src/directives/directives.module.ts
+++ frontend/src/directives/directives.module.ts
@@ -1,14 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { LoadingDirective } from './loading.directive';
+import { TouchDirective } from './touch.directive';
 
 @NgModule({
   imports: [CommonModule],
   declarations: [
     LoadingDirective,
+    TouchDirective,
   ],
   exports: [
     LoadingDirective,
+    TouchDirective
   ],
 })
 export class DirectivesModule {}
