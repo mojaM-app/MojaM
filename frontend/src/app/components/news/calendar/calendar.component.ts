0b09ccfce7be4cb8a2e88c1143dfbaad685467ae
mojaM-app <miriam.rzeszow@gmail.com>
2023-07-31 22:10:06 +0200

    move class CustomDateFormatter into separate file

--- frontend/src/app/components/news/calendar/calendar.component.ts
+++ frontend/src/app/components/news/calendar/calendar.component.ts
@@ -1,102 +1,14 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  Injectable,
-  OnInit,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import {
   CalendarDateFormatter,
   CalendarEvent,
   CalendarView,
   DateAdapter,
-  DateFormatterParams,
   DAYS_OF_WEEK,
 } from 'angular-calendar';
 import { CultureService } from 'src/services/translate/culture.service';
 import { BaseNewsComponent } from '../base-news.component';
-import { formatDate } from '@angular/common';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { distinctUntilChanged } from 'rxjs/operators';
-import { Subscription } from 'rxjs';
-
-@Injectable()
-export class CustomDateFormatter extends CalendarDateFormatter {
-  readonly breakpoint$ = this.breakpointObserver
-    .observe([
-      Breakpoints.Large,
-      Breakpoints.Medium,
-      Breakpoints.Small,
-      '(min-width: 500px)',
-    ])
-    .pipe(distinctUntilChanged());
-
-  readonly subscription: Subscription;
-
-  private weekday: 'long' | 'short' | 'narrow' = 'narrow';
-
-  constructor(
-    dateAdapter: DateAdapter,
-    private _cultureService: CultureService,
-    private breakpointObserver: BreakpointObserver
-  ) {
-    super(dateAdapter);
-
-    this.subscription = this.breakpoint$.subscribe((value) => {
-      return this.breakpointChanged(value);
-    });
-  }
-
-  public override monthViewColumnHeader({
-    date,
-    locale,
-  }: DateFormatterParams): string {
-    return date.toLocaleDateString(locale, {
-      weekday: this.weekday,
-      hourCycle: 'h24',
-    });
-  }
-
-  override dayViewTitle({ date, locale }: DateFormatterParams): string {
-    return date.toLocaleDateString(locale, {
-      weekday: 'long',
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric',
-    });
-  }
-
-  public override weekViewHour({ date, locale }: DateFormatterParams): string {
-    return this.dayViewHour({ date, locale });
-  }
-
-  public override dayViewHour({ date, locale }: DateFormatterParams): string {
-    return formatDate(
-      date,
-      'HH:mm',
-      locale ?? this._cultureService.currentCulture
-    );
-  }
-
-  override weekViewColumnHeader({ date, locale }: DateFormatterParams): string {
-    return this.monthViewColumnHeader({ date, locale });
-  }
-
-  public ngOnDestroy(): void {
-    this.subscription.unsubscribe();
-  }
-
-  private breakpointChanged(arg: any): void {
-    if (this.breakpointObserver.isMatched(Breakpoints.Large)) {
-      this.weekday = 'long';
-    } else if (this.breakpointObserver.isMatched(Breakpoints.Medium)) {
-      this.weekday = 'short';
-    } else if (this.breakpointObserver.isMatched(Breakpoints.Small)) {
-      this.weekday = 'short';
-    } else if (this.breakpointObserver.isMatched('(min-width: 500px)')) {
-      this.weekday = 'narrow';
-    }
-  }
-}
+import { CustomDateFormatter } from './date-formatters/custom.date-formatter';
 
 //https://mattlewis92.github.io/angular-calendar/#/kitchen-sink
 @Component({
---
+++ frontend/src/app/components/news/calendar/date-formatters/custom.date-formatter.ts
@@ -0,0 +1,90 @@
+import { Injectable } from '@angular/core';
+import {
+  CalendarDateFormatter,
+  DateAdapter,
+  DateFormatterParams,
+} from 'angular-calendar';
+import { CultureService } from 'src/services/translate/culture.service';
+import { formatDate } from '@angular/common';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { distinctUntilChanged } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
+
+@Injectable()
+export class CustomDateFormatter extends CalendarDateFormatter {
+  readonly breakpoint$ = this.breakpointObserver
+    .observe([
+      Breakpoints.Large,
+      Breakpoints.Medium,
+      Breakpoints.Small,
+      '(min-width: 500px)',
+    ])
+    .pipe(distinctUntilChanged());
+
+  readonly subscription: Subscription;
+
+  private weekday: 'long' | 'short' | 'narrow' = 'narrow';
+
+  constructor(
+    dateAdapter: DateAdapter,
+    private _cultureService: CultureService,
+    private breakpointObserver: BreakpointObserver
+  ) {
+    super(dateAdapter);
+
+    this.subscription = this.breakpoint$.subscribe((value) => {
+      return this.breakpointChanged(value);
+    });
+  }
+
+  public override monthViewColumnHeader({
+    date,
+    locale,
+  }: DateFormatterParams): string {
+    return date.toLocaleDateString(locale, {
+      weekday: this.weekday,
+      hourCycle: 'h24',
+    });
+  }
+
+  override dayViewTitle({ date, locale }: DateFormatterParams): string {
+    return date.toLocaleDateString(locale, {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    });
+  }
+
+  public override weekViewHour({ date, locale }: DateFormatterParams): string {
+    return this.dayViewHour({ date, locale });
+  }
+
+  public override dayViewHour({ date, locale }: DateFormatterParams): string {
+    return formatDate(
+      date,
+      'HH:mm',
+      locale ?? this._cultureService.currentCulture
+    );
+  }
+
+  override weekViewColumnHeader({ date, locale }: DateFormatterParams): string {
+    return this.monthViewColumnHeader({ date, locale });
+  }
+
+  public ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  private breakpointChanged(arg: any): void {
+    if (this.breakpointObserver.isMatched(Breakpoints.Large)) {
+      this.weekday = 'long';
+    } else if (this.breakpointObserver.isMatched(Breakpoints.Medium)) {
+      this.weekday = 'short';
+    } else if (this.breakpointObserver.isMatched(Breakpoints.Small)) {
+      this.weekday = 'short';
+    } else if (this.breakpointObserver.isMatched('(min-width: 500px)')) {
+      this.weekday = 'narrow';
+    }
+  }
+}
