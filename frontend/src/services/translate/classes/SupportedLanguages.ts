9fa0525085c2c81d5f1774f52cc0f853f5654dff
mojaM-app <miriam.rzeszow@gmail.com>
2024-09-12 21:05:17 +0200

    better ngsw-config

--- frontend/src/services/translate/classes/SupportedLanguages.ts
+++ frontend/src/services/translate/classes/SupportedLanguages.ts
@@ -1,17 +1,18 @@
+import { DateUtils } from 'src/utils/date.utils';
 import { ILanguageData } from '../interfaces/ILanguageData';
 
 export class SupportedLanguages {
   private static _supportedLanguages: ILanguageData[] | null = null;
 
-  static Get(): ILanguageData[] {
+  public static Get(): ILanguageData[] {
     if (this._supportedLanguages === null) {
       this._supportedLanguages = [
         {
           id: 'pl',
           label: 'język polski',
           icon: 'svg/languages/pl.svg',
-          messages: () =>
-            fetch(`./i18n/pl.json?${new Date().getTime()}`)
+          messages: (): Promise<Record<string, any>> =>
+            fetch(`./i18n/pl.json?${DateUtils.toDateString(new Date())}`)
               .then(response => response.json())
               .then(messages => ({ pl: messages })),
         } satisfies ILanguageData,
--- frontend/src/utils/date.utils.ts
+++ frontend/src/utils/date.utils.ts
@@ -2,4 +2,8 @@ export class DateUtils {
   public static toUtcDate(date: Date): Date {
     return new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate(), 0, 0, 0, 0));
   }
+
+  public static toDateString(date: Date): string {
+    return date.toISOString().split('T')[0];
+  }
 }
