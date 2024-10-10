ccd3ca05b1062dab5da51b29f13362185c8b6c4b
mojaM-app <miriam.rzeszow@gmail.com>
2024-10-10 20:22:40 +0200

    calendar component

--- frontend/src/app/components/calendar/calendar.component.ts
+++ frontend/src/app/components/calendar/calendar.component.ts
@@ -1,12 +1,25 @@
-import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  effect,
+  Inject,
+  signal,
+  viewChild,
+} from '@angular/core';
 import {
   CalendarDateFormatter,
+  CalendarDayViewComponent,
   CalendarEvent,
+  CalendarMonthViewComponent,
   CalendarView,
+  CalendarWeekViewComponent,
   DateAdapter,
   DAYS_OF_WEEK,
 } from 'angular-calendar';
+import { ViewPeriod } from 'calendar-utils';
+import { debounceTime } from 'rxjs';
 import { IS_MOBILE } from 'src/app/app.config';
+import { CalendarService } from 'src/services/calendar/calendar.service';
 import { CultureService } from 'src/services/translate/culture.service';
 import { CustomDateFormatter } from './date-formatters/custom.date-formatter';
 
@@ -24,21 +37,44 @@ import { CustomDateFormatter } from './date-formatters/custom.date-formatter';
   ],
 })
 export class CalendarComponent {
+  public readonly weekStartsOn: number = DAYS_OF_WEEK.MONDAY;
+  public readonly weekendDays: number[] = [DAYS_OF_WEEK.SATURDAY, DAYS_OF_WEEK.SUNDAY];
+
   public CalendarView = CalendarView;
-  public view: CalendarView = CalendarView.Month;
-  public viewDate: Date = new Date();
-  public events: CalendarEvent[] = [];
-  public weekStartsOn: number = DAYS_OF_WEEK.MONDAY;
-  public weekendDays: number[] = [DAYS_OF_WEEK.SATURDAY, DAYS_OF_WEEK.SUNDAY];
+  public view = signal<CalendarView>(CalendarView.Month);
+  public viewDate = signal<Date>(new Date());
+  public events = signal<CalendarEvent[]>([]);
+  public locale = signal<string>(CultureService.DefaultCulture);
+
+  private calendarMonth = viewChild(CalendarMonthViewComponent);
+  private calendarWeek = viewChild(CalendarWeekViewComponent);
+  private calendarDay = viewChild(CalendarDayViewComponent);
 
   public constructor(
     @Inject(IS_MOBILE) public isMobile: boolean,
-    public cultureService: CultureService,
-    private _dateAdapter: DateAdapter
-  ) {}
+    cultureService: CultureService,
+    private _dateAdapter: DateAdapter,
+    private _calendarService: CalendarService
+  ) {
+    this.locale.set(cultureService.currentCulture);
+
+    effect(() => {
+      if (
+        (this.calendarMonth() || this.calendarWeek() || this.calendarDay()) &&
+        this.viewDate() &&
+        this.view()
+      ) {
+        setTimeout(() => this.onViewChanged(), 100);
+      }
+    });
+  }
+
+  public viewDateChange(date: Date): void {
+    this.viewDate.set(date);
+  }
 
   public setView(view: CalendarView): void {
-    this.view = view;
+    this.view.set(view);
   }
 
   public setNextView(): void {
@@ -54,8 +90,41 @@ export class CalendarComponent {
       day: this._dateAdapter.addDays,
       week: this._dateAdapter.addWeeks,
       month: this._dateAdapter.addMonths,
-    }[this.view];
+    }[this.view()];
 
-    this.viewDate = addFn(this.viewDate, moveBack ? -1 : 1);
+    this.viewDate.set(addFn(this.viewDate(), moveBack ? -1 : 1));
+  }
+
+  private onViewChanged(): void {
+    const component = this.calendarMonth() || this.calendarWeek() || this.calendarDay();
+    const viewDate = this.viewDate();
+    switch (this.view()) {
+      case CalendarView.Month:
+        this.updateCalendarEvents((component as CalendarMonthViewComponent)?.view?.period);
+        break;
+      case CalendarView.Week:
+        this.updateCalendarEvents((component as CalendarWeekViewComponent)?.view?.period);
+        break;
+      case CalendarView.Day:
+        this.updateCalendarEvents({
+          start: this._dateAdapter.startOfDay(viewDate),
+          end: this._dateAdapter.endOfDay(viewDate),
+          events: [],
+        } satisfies ViewPeriod);
+        break;
+    }
+  }
+
+  private updateCalendarEvents(period?: ViewPeriod): void {
+    if (!period) {
+      return;
+    }
+
+    this._calendarService
+      .getEvents(period!.start, period!.end)
+      .pipe(debounceTime(200))
+      .subscribe((events: CalendarEvent[]) => {
+        this.events.set(events);
+      });
   }
 }
--- frontend/src/services/calendar/calendar.service.ts
+++ frontend/src/services/calendar/calendar.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { CalendarEvent } from 'angular-calendar';
+import { map, Observable } from 'rxjs';
+import { ICalendarEvent } from 'src/interfaces/calendar/calendar-event';
 import { BaseService } from '../common/base.service';
 import { HttpClientService } from '../common/httpClient.service';
 
@@ -8,5 +11,33 @@ import { HttpClientService } from '../common/httpClient.service';
 export class CalendarService extends BaseService {
   public constructor(private _httpClient: HttpClientService) {
     super();
+  }
+
+  public getEvents(start: Date, end: Date) : Observable<CalendarEvent[]> {
+    return this._httpClient
+      .request()
+      .withUrl(this.API_ROUTES.calendar.getEvents())
+      .withParams({ start: start.toISOString(), end: end.toISOString() })
+      .get<ICalendarEvent[]>()
+      .pipe(
+        map((events: ICalendarEvent[]) => {
+          return events.map(event => {
+            return {
+              start: this.toDateTime(event.start),
+              end: this.toDateTime(event.end),
+              title: event.title ?? '',
+              allDay: event.allDay ?? false,
+            } satisfies CalendarEvent;
+          });
+        })
+      );
+  }
+
+  private toDateTime(date: string | null | undefined): Date {
+    if (!date) {
+      return new Date();
+    }
+
+    return new Date(date);
   }
 }
