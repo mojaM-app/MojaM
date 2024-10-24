2ed0602aa117a8280f021156e8022e21792ebcfa
mojaM-app <miriam.rzeszow@gmail.com>
2024-10-24 22:57:34 +0200

    new interface IUserId

--- backend/src/interfaces/index.ts
+++ backend/src/interfaces/index.ts
@@ -5,3 +5,5 @@ export type { IResponse, IResponseError } from './response.interface';
 export type { RequestWithIdentity } from './RequestWithIdentity';
 
 export type { IGridPageResponseDto, IPageData, ISortData } from './grid-page.response.dto';
+
+export type { IUserId } from './IUser.Id';
