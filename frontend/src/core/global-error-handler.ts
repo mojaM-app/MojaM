d02d26fdee8a34cdbd748004a3d47040d689d983
mojaM-app <miriam.rzeszow@gmail.com>
2024-10-26 21:36:01 +0200

    440 error code

--- frontend/src/core/global-error-handler.ts
+++ frontend/src/core/global-error-handler.ts
@@ -2,6 +2,7 @@
 import { ErrorHandler, Injectable } from '@angular/core';
 import { SnackBarService } from 'src/services/snackbar/snack-bar.service';
 import { TranslationService } from 'src/services/translate/translation.service';
+import { StringUtils } from 'src/utils/string.utils';
 
 @Injectable()
 export class GlobalErrorHandler implements ErrorHandler {
@@ -12,6 +13,13 @@ export class GlobalErrorHandler implements ErrorHandler {
 
   public handleError(error: any): void {
     let message = error?.message;
+    if (
+      StringUtils.isString(message) &&
+      (message as string).trim().toLowerCase().startsWith('failed to fetch')
+    ) {
+      message = this._translationService.get('Errors/No_Connection');
+    }
+
     if (error?.status >= 0) {
       switch (error.status) {
         case 0:
@@ -23,6 +31,9 @@ export class GlobalErrorHandler implements ErrorHandler {
         case 401:
         case 403:
           message = this._translationService.get('Errors/Unauthorized');
+          break;
+        case 440:
+          message = this._translationService.get('Errors/Session_Expired');
           break;
       }
     }
