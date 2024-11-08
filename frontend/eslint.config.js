f8184a2913b80e5de3541fd7b41e7b4df8289d69
mojaM-app <miriam.rzeszow@gmail.com>
2024-11-08 20:40:17 +0100

    new eslint rules

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -2,6 +2,7 @@
 const eslint = require('@eslint/js');
 const tseslint = require('typescript-eslint');
 const angular = require('angular-eslint');
+const prettier = require('prettier');
 
 module.exports = tseslint.config(
   {
@@ -23,13 +24,82 @@ module.exports = tseslint.config(
         },
       ],
       "@typescript-eslint/explicit-function-return-type": "error",
-      "@typescript-eslint/explicit-member-accessibility": "error"
+      "@typescript-eslint/explicit-member-accessibility": "error",
       // 'rxjs/no-async-subscribe': 'error',
       // 'rxjs/no-ignored-observable': 'error',
       // 'rxjs/no-ignored-subscription': 'error',
       // 'rxjs/no-nested-subscribe': 'error',
       // 'rxjs/no-unbound-methods': 'error',
       // 'rxjs/throw-error': 'error',
+      //'prettier/prettier': 'warn',
+      'linebreak-style': ['off', 'windows'],
+      'import/prefer-default-export': 'off',
+      'no-underscore-dangle': 'off',
+      'class-methods-use-this': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/member-ordering': [
+        'warn',
+        {
+          default: [
+            'public-decorated-field',
+            'public-field',
+            'protected-field',
+            'private-field',
+            'constructor',
+            'public-method',
+            'protected-method',
+            'private-method',
+          ],
+        },
+      ],
+      '@typescript-eslint/naming-convention': [
+        'warn',
+        {
+          selector: 'classProperty',
+          leadingUnderscore: 'require',
+          modifiers: ['private'],
+          format: ['camelCase'],
+        },
+        {
+          selector: ['variable', 'function', 'classMethod'],
+          format: ['camelCase'],
+          leadingUnderscore: 'forbid',
+        },
+        {
+          selector: 'parameter',
+          format: ['camelCase'],
+          leadingUnderscore: 'allow',
+        },
+        {
+          selector: ['class'],
+          format: ['PascalCase'],
+          leadingUnderscore: 'forbid',
+        },
+        {
+          selector: 'variable',
+          modifiers: ['global', 'const'],
+          format: ['UPPER_CASE'],
+        },
+        {
+          selector: 'memberLike',
+          modifiers: ['private'],
+          format: ['camelCase'],
+          leadingUnderscore: 'require',
+        },
+        {
+          selector: 'typeParameter',
+          format: ['PascalCase'],
+          prefix: ['T'],
+        },
+      ],
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'max-len': 'off',
+      'no-empty-function': 'off',
+      '@typescript-eslint/no-empty-function': 'warn',
+      'no-console': 'off',
+      'no-debugger': 'warn',
+      'prefer-spread': 'off',
+      '@angular-eslint/no-output-native': 'error',
     },
   },
   {
