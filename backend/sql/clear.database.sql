f8535875348b8476d3f3614741862de3e2e359d2
mojaM-app <miriam.rzeszow@gmail.com>
2024-11-27 19:49:05 +0100

    remove vannouncements when deleting all db objects

--- backend/sql/clear.database.sql
+++ backend/sql/clear.database.sql
@@ -1,13 +1,14 @@
 DROP TABLE IF EXISTS
 user_reset_password_tokens,
-user_to_systempermissions, 
-system_permissions, 
+user_to_systempermissions,
+system_permissions,
 users,
 announcements,
 announcement_items;
 
 DROP VIEW IF EXISTS
-vusers;
+vusers,
+vannouncements;
 
 DROP TABLE IF EXISTS _prisma_migrations,
 typeorm_metadata,
