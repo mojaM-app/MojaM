2ed836ba67494a78b5c130e433567c2bd037378e
mojaM-app <miriam.rzeszow@gmail.com>
2025-04-24 22:27:18 +0200

    refactor

--- backend/src/interfaces/index.ts
+++ backend/src/interfaces/index.ts
@@ -2,9 +2,9 @@ export type { IRoutes } from './routes.interface';
 
 export type { IResponse, IResponseError } from './response.interfaces';
 
-export type { IRequestWithIdentity } from './IRequestWithIdentity';
+export type { IRequestWithIdentity } from './request.interfaces';
 
-export type { IGridPageResponseDto, IPageData, ISortData } from './grid-page.response.dto';
+export type { IGridPageResponseDto, IPageData, ISortData } from './grid/grid-page.response.dto';
 
 export type { IHasGuidId } from './IHasGuidId';
 
