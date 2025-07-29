fa5377cf4f78dea78fb3526531e8758d3cbca8ce
mojaM-app <miriam.rzeszow@gmail.com>
2025-07-29 19:18:09 +0200

    fixed eslint warnings

--- backend/src/modules/announcements/tests/test.helpers.ts
+++ backend/src/modules/announcements/tests/test.helpers.ts
@@ -1,5 +1,5 @@
 import { generateRandomDate, generateRandomString } from '@utils';
-import { CreateAnnouncementItemDto, CreateAnnouncementsDto } from '../dtos/create-announcements.dto';
+import { type CreateAnnouncementItemDto, type CreateAnnouncementsDto } from '../dtos/create-announcements.dto';
 
 const generateValidAnnouncements = (): CreateAnnouncementsDto => {
   return {
@@ -7,16 +7,16 @@ const generateValidAnnouncements = (): CreateAnnouncementsDto => {
     title: generateRandomString(40),
     items: [
       {
-        content: '1' + generateRandomString(400),
+        content: `1${generateRandomString(400)}`,
       } satisfies CreateAnnouncementItemDto,
       {
-        content: '2' + generateRandomString(800),
+        content: `2${generateRandomString(800)}`,
       } satisfies CreateAnnouncementItemDto,
       {
-        content: '3' + generateRandomString(1500),
+        content: `3${generateRandomString(1500)}`,
       } satisfies CreateAnnouncementItemDto,
       {
-        content: '4' + generateRandomString(255),
+        content: `4${generateRandomString(255)}`,
       } satisfies CreateAnnouncementItemDto,
     ],
   } satisfies CreateAnnouncementsDto;
