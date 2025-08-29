071b5a0154bd1ae580b8678bcdfc31ef63589b01
mojaM-app <miriam.rzeszow@gmail.com>
2025-08-29 20:08:48 +0200

    check front app update every 5min, whn new version reload page

--- frontend/src/services/sw-update/sw-update.service.ts
+++ frontend/src/services/sw-update/sw-update.service.ts
@@ -1,32 +1,25 @@
 import { Injectable } from '@angular/core';
 import { SwUpdate } from '@angular/service-worker';
 import { interval } from 'rxjs';
-import { TranslationService } from '../translate/translation.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SwUpdateService {
-  public constructor(
-    private readonly _translationService: TranslationService,
-    private readonly _swUpdate: SwUpdate
-  ) {
-    if (_swUpdate.isEnabled) {
-      // Sprawdzaj aktualizacje co 30 sekund
-      interval(30000).subscribe(() => _swUpdate.checkForUpdate());
+  private readonly _updateInterval = 300_000;
 
-      // Nasłuchuj dostępnych aktualizacji
+  public constructor(private readonly _swUpdate: SwUpdate) {
+    if (_swUpdate.isEnabled) {
+      interval(this._updateInterval).subscribe(() => _swUpdate.checkForUpdate());
+
       _swUpdate.versionUpdates.subscribe(event => {
         if (event.type === 'VERSION_READY') {
-          if (confirm(_translationService.get('SwUpdate/NewVersionAvailable'))) {
-            this.updateApp();
-          }
+          this.updateApp();
         }
       });
     }
   }
 
-  // Metoda do wymuszenia sprawdzenia aktualizacji
   public checkForUpdate(): void {
     if (this._swUpdate.isEnabled) {
       this._swUpdate.checkForUpdate();
