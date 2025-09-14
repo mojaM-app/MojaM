bf8fcaf00703bdbdb9738e315213568fd7e934f5
mojaM-app <miriam.rzeszow@gmail.com>
2025-09-14 16:42:33 +0200

    empty PreviewBulletinDayComponent

---
+++ frontend/src/app/components/bulletin/bulletin-preview/preview-bulletin-day/preview-bulletin-day.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-preview-bulletin-day',
+  imports: [],
+  templateUrl: './preview-bulletin-day.component.html',
+  styleUrl: './preview-bulletin-day.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class PreviewBulletinDayComponent {}
